36299d3124cac98233c5234ef9fb78cb305ae353
Asta <astawang0623@gmail.com>
2023-02-03 14:48:21 +0800

    only adain

--- intelli_qr_streamlit.py
+++ intelli_qr_streamlit.py
@@ -56,7 +56,8 @@ with tab_1: # Styled QR Code
             
     # Pick NST method
     st.subheader('Select a NST method: ')
-    nst_method = st.selectbox('select NST method', ('Stytr2', 'AdaIN', 'Mast', 'SANet'))
+    # nst_method = st.selectbox('select NST method', ('Stytr2', 'AdaIN', 'Mast', 'SANet'))
+    nst_method = st.selectbox('select NST method', ('AdaIN'))
 
     if upload_content_img is not None and upload_style_img is not None and qr_code is not None:
         
